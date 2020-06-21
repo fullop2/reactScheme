8634da565124478e7c29e964a4a325cac4466abd
fullop2 <taehyun.park.fullop@gmail.com>
2020-06-22 03:08:05 +0900

    Updates

---
+++ precache-manifest.ad0d88207d65efb0449af477e5f8e3a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a653430dbe489badc01824ecf7761d2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28a4b62ca6786187cec9",
+    "url": "/static/css/main.c7fa6117.chunk.css"
+  },
+  {
+    "revision": "6ec4c361e05791cc8b86",
+    "url": "/static/js/2.0eeadc5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0eeadc5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a4b62ca6786187cec9",
+    "url": "/static/js/main.73a83c54.chunk.js"
+  },
+  {
+    "revision": "b8309373e9d2ded14fe5",
+    "url": "/static/js/runtime-main.b3b38e88.js"
+  }
+]);
